17c3208ed10b1d88eab8c88713de3501ce0e08c6
Nurr-ann <nuranamr736@gmail.com>
2023-10-05 18:03:10 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  * Return: Alawys 0 (Success)
  */
 int main(void)
-{
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+{	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
