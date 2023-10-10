926abc8cee6ecfd5b90e0e8243c7d9014ab7d086
Nurr-ann <nuranamr736@gmail.com>
2023-10-10 16:24:43 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Description: prints _putcher using putcher prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);	
