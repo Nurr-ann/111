59aba2f3f25d2c4637f1df7df50731731b47e2d7
Nurr-ann <nuranamr736@gmail.com>
2023-10-10 17:28:43 -0700

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
- * Description: prints _putcher using putcher prototype
- *
- * Return: Always 0 (Success)
-*/
+ * Return: Always 0 (success)
+ */
 
 int main(void)
-	char str[] = "_putchar";
-	int ch;
+{
 
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar('\n');
+printf("_putchar\n");
 
-	return (0);	
+return (0);
+}
+	
